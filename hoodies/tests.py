42256433a562ce4cb4e3add3bccc1ecc0e120d93
HabeshaQueen <adhoadhigal.com>
2019-05-30 05:57:40 +0300

    add urls path

--- hoodies/tests.py
+++ hoodies/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class NeighbourHoodTestClass(TestCase):
+    """tests to test if NeighbourHood class is been instansiated correctly"""
+    def setUp(self):
+
+        self.new_user = User(username = "adho", email = "adhoadhigal@gmail.com",password = "chukula13")
+        self.new_user.save()
+
+class UserTestClass(TestCase):
+    """tests to test if User class is been instansiated correctly"""
+    def setUp(self):
+        self.new_user = User(username = "adho", email = "adhoadhigal@gmail.com",password = "chukula13")
+        self.new_user.save()
+
+class NeighbourHoodTestClass(TestCase):
+    """tests to test if Business class is been instansiated correctly"""
+    def setUp(self):
+        self.new_user = User(username = "adho", email = "adhoadhigal@@gmail.com",password = "chukula13")
+        self.new_user.save()
---
+++ hoodies/url.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url,include
+from . import views
+
+urlpatterns= [
+    url(r'^$',views.location, name="location"),
+    url(r'(\d+)',views.homm, name="home"),
+    url(r'register/', views.register, name="register"),
+    url(r'accounts/', include('django.contrib.auth.urls')),
+    url(r'hoodies/', views.hoodies, name="hoodies"),
+    url(r'post',views.post,name="post"),
+    url(r'^search/', views.search_business, name='search_results'),
+    url(r'profile/',views.profile_index, name='profile'),
+    url(r'new/',views.update_index, name='new'),
+    url(r'acti/',views.acti, name='acti')
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
